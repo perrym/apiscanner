0f11e89c99abc4c6f6b7d4e371ae9d8334f23811
perrym <67121078+perrym@users.noreply.github.com>
2025-08-10 19:03:20 +0200

    Update swagger_generator.py

--- swagger_generator.py
+++ swagger_generator.py
@@ -1,5 +1,5 @@
 
-# Ultimate Swagger Generator (Improved v2)
+# Ultimate Swagger Generator (Improved v3)
 #########################################
 # APISCAN - API Security Scanner        #
 # Licensed under the MIT License        #
